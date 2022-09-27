87649788ac539969bbdb5823a8fa5342f19188c6
Ankit <ankitGhosh456@gmail.com>
2022-09-27 11:27:31 +0530

    find maxi

--- src/com/codinclub/MaximumNumber.java
+++ src/com/codinclub/MaximumNumber.java
@@ -2,15 +2,19 @@ package com.codinclub;
 
 import java.lang.constant.Constable;
 
-public class MaximumNumber {
-    public static String findMaximumNumber(String a, String b, String c ) {
-        String max = a;
-        if (b.compareTo(max) > 0)
-            max = b; // b is the largest
+public class MaximumNumber<E extends Comparable <E>> {
+    E a, b, c;
 
-        if (c.compareTo(max) > 0)
-            max = c; // c is the largest
+    public static <E extends Comparable<E>> E maximumIs(E a, E b, E c) {
+        {
+            E max = a;
+            if (b.compareTo(max) > 0)
+                max = b; // b is the largest
 
-        return max; // returns the largest object
+            if (c.compareTo(max) > 0)
+                max = c; // c is the largest
+
+            return max; // returns the largest object
+        }
     }
 }
--- src/com/codinclub/main.java
+++ src/com/codinclub/main.java
@@ -2,6 +2,8 @@ package com.codinclub;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("maximum string is :"+ MaximumNumber.findMaximumNumber("apple,","peach","banana"));
+        System.out.println("maxi number is :"+MaximumNumber.maximumIs(15433,71655,9875));
+        System.out.println("maxi number is :"+MaximumNumber.maximumIs(15433.5f, 71655.5f,9875.6f));
+        System.out.println("maxi number is :"+MaximumNumber.maximumIs("apple","peach","banana"));
     }
 }
