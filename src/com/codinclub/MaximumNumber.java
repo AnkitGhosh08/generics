9e681ff7fd995a80692883d9233614a49a1ebc38
Ankit <ankitGhosh456@gmail.com>
2022-09-27 11:10:23 +0530

    maxi string

--- src/com/codinclub/MaximumNumber.java
+++ src/com/codinclub/MaximumNumber.java
@@ -1,15 +1,16 @@
 package com.codinclub;
 
+import java.lang.constant.Constable;
+
 public class MaximumNumber {
-    public static float findMaximumNumber(float a, float b, float c) {
-        float max = a;
-        if (b > max)
+    public static String findMaximumNumber(String a, String b, String c ) {
+        String max = a;
+        if (b.compareTo(max) > 0)
             max = b; // b is the largest
 
-        if (c > max)
+        if (c.compareTo(max) > 0)
             max = c; // c is the largest
 
         return max; // returns the largest object
-
     }
 }
--- src/com/codinclub/main.java
+++ src/com/codinclub/main.java
@@ -2,6 +2,6 @@ package com.codinclub;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("maximum Number is :"+ MaximumNumber.findMaximumNumber(505.5f,910.6f,877.7f));
+        System.out.println("maximum string is :"+ MaximumNumber.findMaximumNumber("apple,","peach","banana"));
     }
 }
